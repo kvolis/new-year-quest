84bd7d8687488daa783d06ffc446bab1755aa7be
aakislov <aakislov@cyberry.su>
2025-12-26 01:43:55 +0300

    С Новым Годом, коллеги!

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import hashlib
 
-# не изменяйте это значение
-TARGET = "3c8aed7d3d1699163349850b93014642"
+# не изменяйте эти значение
+TARGET_START = "e8c48234fd"
+TARGET_END = "d2e3e0c962"
+
 
 def main():
     i = 0
     while i < 2147483647:
+
         digest = hashlib.md5(str(i).encode()).hexdigest()
-        if digest == TARGET:
+
+        if digest.startswith(TARGET_START) and digest.endswith(TARGET_END):
             print(f"Найдено: {l}")
             return
 
